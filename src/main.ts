bfd05ab146b6694aab80e603495a14773b7a1073
RoboticRoss <rmgoldbaum@gmail.com>
2025-10-19 15:53:02 -0400

    temp

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Ross's Website</h1>
+    <h1>Ross's Website yupdated</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
